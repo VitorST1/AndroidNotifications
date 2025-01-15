1bbca7dd8abb54ce8e2943c62a826c1540948aa2
Vitor <vitorturqueti@hotmail.com>
2025-01-15 13:25:57 -0300

    fix missing dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,5 +32,12 @@ android {
 }
 
 dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
     implementation(libs.core.ktx)
 }
